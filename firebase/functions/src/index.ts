85bbc769ea3e3bc29255b33893896f396ad58f80
Tim Rasim <neogucky@gmail.com>
2018-10-01 23:33:05 +0200

    Added firebase bacxkend

---
+++ firebase/functions/src/index.ts
@@ -0,0 +1,37 @@
+import * as functions from 'firebase-functions';
+
+import * as admin from 'firebase-admin';
+admin.initializeApp();
+
+
+exports.newLunchDate = functions.firestore
+    .document('suggestions/{suggestionId}')
+    .onCreate((change, context) => {
+
+
+    
+    const data = change.data();
+
+    const id = data.id
+	
+	const time = data.time.toDate();
+    const timeFormatted = time.getHours() + ":" + time.getMinutes();
+
+	
+    // Notification content
+    const payload = {
+      notification: {
+          title: 'Lunch at ' + timeFormatted,
+          body: 'New lunch date available at ' + timeFormatted,
+          icon: 'https://goo.gl/Fz9nrQ'
+      }
+    }
+
+	const options = {
+        priority: "high",
+        timeToLive: 60*60*2
+    };
+
+	return admin.messaging().sendToTopic("IMIS", payload, options);
+
+});
