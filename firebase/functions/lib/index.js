e619c53a0fb91b4b0eef3c7ebab2c0e53bcadd44
Tim Rasim <neogucky@gmail.com>
2018-10-18 21:08:07 +0200

    removed data scraper due to firebase spark plan limits

--- firebase/functions/lib/index.js
+++ firebase/functions/lib/index.js
@@ -1,4 +1,7 @@
 "use strict";
+//const rp = require('request-promise');
+//const cheerio = require('cheerio');
+//const request = require('request');
 Object.defineProperty(exports, "__esModule", { value: true });
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
@@ -7,21 +10,63 @@ exports.newLunchDate = functions.firestore
     .document('suggestions/{suggestionId}')
     .onCreate((change, context) => {
     const data = change.data();
-    const id = data.id;
-    const time = data.time.toDate();
-    const timeFormatted = (time.getHours() + 2) + ":" + time.getMinutes();
-    // Notification content
-    const payload = {
-        notification: {
-            title: 'Lunch at ' + timeFormatted,
-            body: 'New lunch date available at ' + timeFormatted,
-            icon: 'https://goo.gl/Fz9nrQ'
-        }
-    };
+    let payload;
+    if (data.type === undefined || data.type != 'now') {
+        //assuming datatype 'time' with fallback to earlier versions without type
+        const time = data.time.toDate();
+        const timeFormatted = (time.getHours() + 2) + ":" + time.getMinutes();
+        payload = {
+            notification: {
+                title: 'Lunch at ' + timeFormatted,
+                body: 'New lunch date available at ' + timeFormatted,
+                icon: 'https://goo.gl/Fz9nrQ',
+                sound: 'default'
+            }
+        };
+    }
+    else {
+        payload = {
+            notification: {
+                title: 'Hungry?',
+                body: data.creator + ' wants to go for lunch in 5 minutes',
+                icon: 'https://goo.gl/Fz9nrQ',
+                sound: 'default'
+            }
+        };
+    }
     const options = {
         priority: "high",
         timeToLive: 60 * 60 * 2
     };
     return admin.messaging().sendToTopic("IMIS", payload, options);
 });
+/*
+exports.loadMenus = functions.https.onRequest((req, res) => {
+
+    //FIXME: I would like a dynamic parser with which I create a database entry on how to parse listed websites
+    
+    //Get Mensa Lübeck
+    const url = 'https://www.studentenwerk.sh/de/essen/standorte/luebeck/mensa-luebeck/speiseplan.html';
+    const options = {
+        uri: url,
+        headers: { 'User-Agent': 'test' },
+        transform: (body) => cheerio.load(body)
+    }
+    
+    rp(options)
+        .then(($) => {
+            const scrapedDays = $('#days').find('div.day>table');
+            
+            for (let day in scrapedDays){
+                admin.firestore().collection('menu').add({
+                  'cantineID': 'mensa',
+                  'name': day,
+                  'price': '80 Cent'
+                }).catch(err => console.log(err));
+            }
+            res.status(200).end();
+        })
+        .catch((err) => res.status(400).send(err));
+});
+*/
 //# sourceMappingURL=index.js.map
--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,5 +1,8 @@
-import * as functions from 'firebase-functions';
+//const rp = require('request-promise');
+//const cheerio = require('cheerio');
+//const request = require('request');
 
+import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 admin.initializeApp();
 
@@ -9,22 +12,31 @@ exports.newLunchDate = functions.firestore
     .onCreate((change, context) => {
    
     const data = change.data();
-
-    const id = data.id
+	let payload;
 	
-	const time = data.time.toDate();
+	if (data.type === undefined || data.type != 'now'){
+		//assuming datatype 'time' with fallback to earlier versions without type
+		const time = data.time.toDate();
+		const timeFormatted = (time.getHours() + 2) + ":" + time.getMinutes();
 		
-    const timeFormatted = (time.getHours() + 2) + ":" + time.getMinutes();
-
-	
-    // Notification content
-    const payload = {
-      notification: {
-          title: 'Lunch at ' + timeFormatted,
-          body: 'New lunch date available at ' + timeFormatted,
-          icon: 'https://goo.gl/Fz9nrQ'
-      }
-    }
+		payload = {
+		  notification: {
+			  title: 'Lunch at ' + timeFormatted,
+			  body: 'New lunch date available at ' + timeFormatted,
+			  icon: 'https://goo.gl/Fz9nrQ',
+			  sound: 'default'
+		  }
+		}
+	} else {
+		payload = {
+		  notification: {
+			  title: 'Hungry?',
+			  body: data.creator + ' wants to go for lunch in 5 minutes',
+			  icon: 'https://goo.gl/Fz9nrQ',
+			  sound: 'default'
+		  }
+		}
+	}
 
 	const options = {
         priority: "high",
@@ -34,3 +46,33 @@ exports.newLunchDate = functions.firestore
 	return admin.messaging().sendToTopic("IMIS", payload, options);
 
 });
+
+/*
+exports.loadMenus = functions.https.onRequest((req, res) => {
+
+	//FIXME: I would like a dynamic parser with which I create a database entry on how to parse listed websites 
+    
+	//Get Mensa Lübeck
+	const url = 'https://www.studentenwerk.sh/de/essen/standorte/luebeck/mensa-luebeck/speiseplan.html';
+    const options = {
+        uri: url,
+        headers: { 'User-Agent': 'test' },
+        transform: (body) => cheerio.load(body)
+    }
+	
+	rp(options)
+        .then(($) => {
+            const scrapedDays = $('#days').find('div.day>table');
+			
+			for (let day in scrapedDays){
+				admin.firestore().collection('menu').add({
+				  'cantineID': 'mensa',
+				  'name': day,
+				  'price': '80 Cent'
+				}).catch(err => console.log(err));
+			} 
+			res.status(200).end();
+        })
+        .catch((err) => res.status(400).send(err));
+});
+*/
