905b2c1c64413c9ec20d798269a8fcbb751d2739
Tim Rasim <neogucky@gmail.com>
2018-10-22 19:43:27 +0100

    updated backend to push notifications to individual devices

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -5,7 +5,7 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 admin.initializeApp();
-
+const db = admin.firestore();
 
 exports.newLunchDate = functions.firestore
     .document('suggestions/{suggestionId}')
@@ -14,7 +14,13 @@ exports.newLunchDate = functions.firestore
     const data = change.data();
 	let payload;
 	
-	if (data.type === undefined || data.type != 'now'){
+	const options = {
+        priority: "high",
+        timeToLive: 60*60*2
+    };
+	
+	if (data.type === undefined || data.type !== 'now'){
+				
 		//assuming datatype 'time' with fallback to earlier versions without type
 		const time = data.time.toDate();
 		const timeFormatted = (time.getHours() + 2) + ":" + time.getMinutes();
@@ -23,56 +29,68 @@ exports.newLunchDate = functions.firestore
 		  notification: {
 			  title: 'Lunch at ' + timeFormatted,
 			  body: 'New lunch date available at ' + timeFormatted,
-			  icon: 'https://goo.gl/Fz9nrQ',
+			  icon: 'icon',
+			  badge: '1',
 			  sound: 'default'
 		  }
 		}
 	} else {
+		//don't show this for a long time
+		options.timeToLive = 60*15;
 		payload = {
 		  notification: {
 			  title: 'Hungry?',
 			  body: data.creator + ' wants to go for lunch in 5 minutes',
-			  icon: 'https://goo.gl/Fz9nrQ',
+			  icon: 'icon',
+			  badge: '1',
 			  sound: 'default'
 		  }
 		}
 	}
 
-	const options = {
-        priority: "high",
-        timeToLive: 60*60*2
-    };
-
-	return admin.messaging().sendToTopic("IMIS", payload, options);
-
+	return pushToAll(payload);
 });
 
-/*
-exports.loadMenus = functions.https.onRequest((req, res) => {
+function loadUsers() {
+	console.log('send push to:');
+	const participantsRef = db.collection('participants');
+	const defer = new Promise((resolve, reject) => {
+		const users = [];
+		participantsRef.where('allowPush', '==', true).get()
+		.then(snapshot => {
+		  snapshot.forEach(doc => {
+			const participant = doc.data();
+			//FIXME: (participant.busyUntil === undefined || participant.busyUntil.seconds < now.getSeconds())
+			if (participant.FCMtoken !== undefined && (participant.suggestionID !== -1)){
+				console.log(participant.name);
+				users.push(participant);
+			}
+		  });
+		  resolve(users);
+		}).catch(err => {
+		  console.log('Error getting documents', err);
+		});
+	}).catch(err => {
+		  console.log('Error getting documents', err);
+		});
+	return defer;
+}
 
-	//FIXME: I would like a dynamic parser with which I create a database entry on how to parse listed websites 
-    
-	//Get Mensa Lübeck
-	const url = 'https://www.studentenwerk.sh/de/essen/standorte/luebeck/mensa-luebeck/speiseplan.html';
-    const options = {
-        uri: url,
-        headers: { 'User-Agent': 'test' },
-        transform: (body) => cheerio.load(body)
-    }
+//sends a push notifications to all users that have not opted out
+function pushToAll(payload){
 	
-	rp(options)
-        .then(($) => {
-            const scrapedDays = $('#days').find('div.day>table');
-			
-			for (let day in scrapedDays){
-				admin.firestore().collection('menu').add({
-				  'cantineID': 'mensa',
-				  'name': day,
-				  'price': '80 Cent'
-				}).catch(err => console.log(err));
-			} 
-			res.status(200).end();
-        })
-        .catch((err) => res.status(400).send(err));
-});
-*/
+	return loadUsers().then((users : any) => {
+        const tokens = [];
+        for (let it in users) {
+            tokens.push(users[it].FCMtoken);
+        }
+		
+		if (tokens === undefined || tokens.length === 0){
+			console.error('No users selcted to send push to');
+			return new Promise((resolve, reject) => {resolve()});
+		} 
+		
+        return admin.messaging().sendToDevice(tokens, payload);
+    });
+
+}
